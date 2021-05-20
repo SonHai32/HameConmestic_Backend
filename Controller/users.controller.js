051969b1ec5e386df986945b78a212a1f1648454
Lâm Sơn Hải <lamsonhai1379@gmail.com>
2021-05-20 13:54:22 +0700

    fixed undefined userinfo

--- Controller/users.controller.js
+++ Controller/users.controller.js
@@ -3,10 +3,10 @@ export default class UserCtrl{
     static async apiPushUser(req, res, next){
         try {
             const userInfo = {
-                username: req.query.username,
-                password: req.query.password,
-                phoneNumber: req.query.phoneNumber,
-                emailAddress: req.query.emailAddress
+                username: req.query.username || req.body.username,
+                password: req.query.password || req.body.password,
+                phoneNumber: req.query.phoneNumber || req.body.phoneNumber,
+                emailAddress: req.query.emailAddress || req.body.emailAddress
             }
            let result = await UserDAO.pushUser(userInfo) 
            res.json(result)
